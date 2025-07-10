bb42a1bb34c209684e9b28d7b1fbf7d33aa6b5c8
Sam Bixel <bixel.18@osu.edu>
2025-07-10 16:40:13 -0400

    All data scraped

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from scraper import get_fight_links
+from scraper import get_fight_links, get_fight_stats
 
 
 app = Flask(__name__)
@@ -18,7 +18,14 @@ def get_fighters():
     url = data['cardURL']
     links = get_fight_links(url)
 
-    return jsonify({"status": "success", "received": links})
+    all_fight_data = []
+    # for link in links:
+    #     fight_data = get_fight_stats(link)
+    #     if fight_data:
+    #         all_fight_data.append(fight_data)
+    # return jsonify({"status": "success", "received": links})
+    link = "http://www.ufcstats.com/fight-details/12cedec11b37ddc0"
+    return jsonify({"status": "True", "stats": get_fight_stats(link)})
 
 
 if __name__ == "__main__":
