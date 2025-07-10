6a303b71e9ea064488fc583c525d7144d2eaab56
Sam Bixel <bixel.18@osu.edu>
2025-07-10 18:53:29 -0400

    all data prep, needs cleaned

--- BixPix.py
+++ BixPix.py
@@ -8,19 +8,6 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
 }
 
-# res = requests.get(url, headers=headers)
-# # Makes HTML easily accessible for searching
-# soup = BeautifulSoup(res.text, "html.parser")
-
-# # Get the Dropdown from ESPN
-# dropdowns = soup.find_all("select", class_="dropdown__select")
-# correct_dropdown = next(
-#     (dropdown for dropdown in dropdowns if any(len(option.get("value", "")) == 9 for option in dropdown.find_all("option"))),
-#     None  # fallback if no match is found
-# )
-# # Make ESPN's dropdown a string
-# dropdown_html = str(correct_dropdown)
-
 res = requests.get("http://www.ufcstats.com/statistics/events/upcoming", headers=headers)
 soup = BeautifulSoup(res.text, "html.parser")
 
@@ -57,18 +44,6 @@ html_output = html_template.replace("<!--DropDown Insert-->", html)
 # Write over the original index.html with index template, except the insertion has been replaced with the dropdown.
 with open("index.html", "w") as file:
     file.write(html_output)
-
-    
-
-
-
-# with open("index_template.html", "r") as file:
-#     html_template = file.read()
-# # Replace the insertion point with espn's exact dropdown html She my rider for a reason
-# html_output = html_template.replace("<!--DropDown Insert-->", dropdown_html)
-# # Write over the original index.html with index template, except the insertion has been replaced with the dropdown.
-# with open("index.html", "w") as file:
-#     file.write(html_output)
 
 # Do the same process for events page.
 with open("event_template.html", "r") as file:
